2e9d8ee5f5df9c3d6fb88e337ecf634bf85063e9
kalkidanh <kalkidanh89@gmail.com>
2022-09-25 06:02:36 -0700

    esrever

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 for char in range(ord('z'), ord('a') - 1, -1):
     print('{}'.format(chr(char - ord('a') + (char % 2) * ord('A') +
-         ((char + 1) % 2) * ord('a'))), end='')
+                         ((char + 1) % 2) * ord('a'))), end='')
