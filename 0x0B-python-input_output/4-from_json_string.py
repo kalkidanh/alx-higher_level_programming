9ac01c2991e438869f40ff74d9bd9c8352b617cc
kalkidanh <kalkidanh89@gmail.com>
2022-10-25 09:06:02 -0700

    JSON loads

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+"""Defines a JSON-to-object function"""
+import json
+
+
+def from_json_string(my_str):
+    """Returns the python object representation of a JSON string."""
+    return json.loads(my_str)
