45d81fb49840bcaec00f7ba26f1be885e007091f
kalkidanh <kalkidanh89@gmail.com>
2022-10-25 10:17:55 -0700

    attribtes replaced

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+
+"""Define the class Student."""
+
+
+class Student:
+    """Represents a student."""
+
+    def __init__(self, first_name, last_name, age):
+        """initialize a new student.
+
+        Args:
+            first_name (str): first name of student.
+            last_name (str): last name of student.
+            age (int): age of student.
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Get a dictionary representation of the student.
+
+        If attrs is a list of strings, represents only those attributes
+        included in the list.
+
+        Args:
+            attrs (list): (Optional) The attributes to represent.
+        """
+        if (type(attrs) == list and
+                all(type(ele) == str for ele in attrs)):
+            return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
+        return self.__dict__
+
+    def reload_from_json(self, json):
+        """Replace all attributes of student.
+
+        Args:
+            json (dict): The key/value pairs to replace.
+        """
+        for k, v in json.items():
+            setattr(self, k, v)
