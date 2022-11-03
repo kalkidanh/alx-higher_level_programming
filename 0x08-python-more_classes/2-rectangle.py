a9a4545b6be35f79900d768fb6ea2b220739260a
kalkidanh <kalkidanh89@gmail.com>
2022-11-03 07:54:22 -0700

    fixed perimeter function

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -51,6 +51,6 @@ class Rectangle:
 
     def perimeter(self):
         """returns the perimeter of the rectangle"""
-        of self.__width == 0 or self.__height == 0:
+        if self.__width == 0 or self.__height == 0:
             return 0
-        return (self.__width * 2) + (self.__height * 2)
+        return ((self.__width * 2) + (self.__height * 2))
