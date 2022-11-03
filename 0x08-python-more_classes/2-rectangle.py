243fbe503ae1fc0c341c618a5c7d26e6d61a6196
kalkidanh <kalkidanh89@gmail.com>
2022-11-03 08:07:18 -0700

    height setter fix

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -52,5 +52,5 @@ class Rectangle:
     def perimeter(self):
         """returns the perimeter of the rectangle"""
         if self.__width == 0 or self.__height == 0:
-            return 0
+            return (0)
         return ((self.__width * 2) + (self.__height * 2))
