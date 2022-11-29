41d4c9e0b4cc85aa50ddcf4f352ff20604f2a983
kalkidanh <kalkidanh89@gmail.com>
2022-11-29 10:36:02 -0800

    indent fix

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,8 +1,8 @@
 -- Create a second table in the hbtn_0c_0 database and insert values.
 CREATE TABLE IF NOT EXISTS second_table (
-	id INT,
-	name VARCHAR(256).
-	score INT
+  id INT,
+  name VARCHAR(256).
+  score INT
 );
 INSERT INTO `second_table` (`id, `name`, `score`) VALUES(1, "John", 10);
 INSERT INTO `second_table` (`id, `name`, `score`) VALUES(2, "Alex", 3);
