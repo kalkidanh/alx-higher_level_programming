94901da628b7b024538abf740b3c8f21f1a2654c
kalkidanh <kalkidanh89@gmail.com>
2022-12-20 08:59:18 -0800

    x times

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+const { argv } = require('process');
+const x = Number(argv[2]);
+const display = () => {
+	for (let i =0; i < x; i++) {
+		console.log('C is fun');
+	}
+};
+isNaN(x)
+? (console.log('Missing number of occurrences'))
+: (display());
