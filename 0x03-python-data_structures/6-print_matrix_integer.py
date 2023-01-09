9343f9f5ffbc1db5a3c074b1cbfb4e77a583c3c5
kalkidanh <kalkidanh89@gmail.com>
2023-01-09 16:21:01 +0000

    matrix printed

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+"""function that prints a matrix of integers"""
 def print_matrix_integer(matrix=[[]]):
     for i in range(len(matrix)):
         for j in range(len(matrix[i])):
