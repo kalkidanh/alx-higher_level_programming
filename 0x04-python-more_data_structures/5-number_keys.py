38131e17d7991e340ca507ee27f2d44df9b612e4
kalkidanh <kalkidanh89@gmail.com>
2023-01-11 16:54:25 +0000

    using len instead

--- 0x04-python-more_data_structures/5-number_keys.py
+++ 0x04-python-more_data_structures/5-number_keys.py
@@ -1,9 +1,3 @@
 #!/usr/bin/python3
 def number_keys(a_dictionary):
-    num = 0
-    list_key = list(a_dictionary.keys())
-
-    for i in list_keys:
-        num += 1
-
-    return (num)
+    return len(a_dictionary.keys())
