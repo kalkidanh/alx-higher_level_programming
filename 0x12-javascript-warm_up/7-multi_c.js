c6b9b3dd242169c18ccf3d45ed8108188a549ad3
kalkidanh <kalkidanh89@gmail.com>
2023-04-10 22:24:15 +0300

    semi fixed

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -2,10 +2,10 @@
 const { argv } = require('process');
 const x = Number(argv[2]);
 const display = () => {
-	for (let i =0; i < x; i++) {
-		console.log('C is fun');
-	}
+  for (let i = 0; i < x; i++) {
+    console.log('C is fun');
+  }
 };
 isNaN(x)
-? (console.log('Missing number of occurrences'))
-: (display());
+  ? (console.log('Missing number of occurrences'))
+  : (display());
