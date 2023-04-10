4dd51aed328116fd93284fa9113a6c97158d6ce6
kalkidanh <kalkidanh89@gmail.com>
2023-04-10 22:30:05 +0300

    compliant

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,13 +1,13 @@
 #!/usr/bin/node
 
 if (isNaN(process.argv[2])) {
-	console.log('Missing size');
+  console.log('Missing size');
 } else {
-	for (let i = 0; i < parseInt(process.argv[2]); i++) {
-		let s = '';
-		for (let j = 0; j < parseInt(process.argv[2]); j++) {
-			s += 'X';
-		}
-		console.log(s);
-	}
+  for (let i = 0; i < parseInt(process.argv[2]); i++) {
+    let s = '';
+    for (let j = 0; j < parseInt(process.argv[2]); j++) {
+      s += 'X';
+    }
+    console.log(s);
+  }
 }
