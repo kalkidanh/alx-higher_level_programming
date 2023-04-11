f58b189ae63aaac717a22529bdaac69ca893f41a
kalkidanh <kalkidanh89@gmail.com>
2023-04-11 20:23:08 +0300

    double function added

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -17,4 +17,9 @@ module.exports = class Rectangle {
   rotate () {
     [this.width, this.height] = [this.height, this.width];
   }
+
+  double () {
+    this.width *= 2;
+    this.height *= 2;
+  }
 };
