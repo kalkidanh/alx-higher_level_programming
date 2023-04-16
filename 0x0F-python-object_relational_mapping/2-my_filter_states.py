95fde454445f89778ea1aec3f28647f786618054
kalkidanh <kalkidanh89@gmail.com>
2023-04-16 20:42:33 +0300

    pycodestyle fix

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Script that lists all states from the database hbtn_0e_0_usa 
+"""Script that lists all states from the database hbtn_0e_0_usa
 that matches a giver argument
 """
 
