fba9f8fdf71326341b13d2c2bdce5d7ff0f32ad7
kalkidanh <kalkidanh89@gmail.com>
2023-04-20 21:16:36 +0300

    typo fix

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -8,7 +8,7 @@ from sqlalchemy.orm import sessionmaker
 
 if __name == "__main__":
     """Gets the state of the DB"""
-    engine = create_engine('mysql+mysqldb://{}:[]@localhost/{}'.format(
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
         argv[1],
         argv[2],
         argv[3]), pool_pre_ping=True)
