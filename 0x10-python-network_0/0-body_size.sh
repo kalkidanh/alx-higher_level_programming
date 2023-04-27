732156c40f7387a336fd8db3462027b6ce9e432c
kalkidanh <kalkidanh89@gmail.com>
2023-04-27 23:35:35 +0300

    comments fixed

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-# Script that takes a URL as an argument and returns the number of bytes in the body
-# of the response.
+# Script that takes a URL as an argument and returns the number of bytes in the body of the response.
 curl -s "$1" | wc -c
