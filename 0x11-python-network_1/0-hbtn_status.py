9db62b11a97d366a8f14a783ba85e7af0cb16471
kalkidanh <kalkidanh89@gmail.com>
2023-04-28 19:55:26 +0300

    typo fixed

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -2,10 +2,10 @@
 """Script that fetches the webpage using urllib module"""
 from urllib import request
 
-if __name__ == "__main__":
-    with request.urlopen("https://intranet.hbtn.io/status") as page:
-        page = page.read()
-        print("Body response:")
-        print("\t- type: {}".format(type(page)))
-        print("\t- content: {}".format(page))
-        print("\t- utf8 content: {}".format(page.decode(encoding='utf-8')))
+if __name__ == '__main__':
+    with request.urlopen('https://alx-intranet.hbtn.io/status') as response:
+        page = response.read()
+        print('Body response:')
+        print('\t- type: {}'.format(type(page)))
+        print('\t- content: {}'.format(page))
+        print('\t- utf8 content: {}'.format(page.decode('utf8')))
