eb5b804e77f636ad893fc023458006a4e1af038e
kalkidanh <kalkidanh89@gmail.com>
2023-04-28 20:56:25 +0300

    error code displayed

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,14 @@
+#!/usr/bin/pythton3
+"""Script that takes a URL sends a rrequest and displays the body
+of the response"""
+
+from sys import argv
+from urllib.request import urlopen, Request
+from urllib.error import HTTPError
+
+if __name__ == '__main__':
+    try:
+        with urlopen(Request(argv[1])) as request:
+            print(request.read().decode('utf-8'))
+    except HTTPError as e:
+        print("Error code: {}".format(e.code))
