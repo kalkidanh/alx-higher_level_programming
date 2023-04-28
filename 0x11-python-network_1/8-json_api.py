5b8412efa66eae853795a9a8617a2ca9799f78af
kalkidanh <kalkidanh89@gmail.com>
2023-04-28 21:35:52 +0300

    request POSTed

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""script that takes in a letter and sends a POST request to
+http://0.0.0.0:5000/search_user with the letter as a parameter."""
+
+from sys import argv
+import requests
+
+if __name == "__main__":
+    if len(argv) == 1:
+        q = ""
+    else:
+        q = argv[1]
+    request = requests.post('http://0.0.0:5000/search_user', data={'q': q})
+    try:
+        req = request.json()
+        if req == {}:
+            print("No result")
+        else:
+            print("[{}] {}".format(req.get("id"), req.get("name")))
+        except ValueError:
+            print("Not a valid JSON")
