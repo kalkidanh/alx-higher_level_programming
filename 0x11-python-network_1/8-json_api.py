262f87ec5378ae88445fd5dca8b14a4a6b7788be
kalkidanh <kalkidanh89@gmail.com>
2023-04-28 21:46:54 +0300

    indentation fix

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -17,5 +17,5 @@ if __name__ == "__main__":
             print("No result")
         else:
             print("[{}] {}".format(req.get("id"), req.get("name")))
-        except ValueError:
-            print("Not a valid JSON")
+    except ValueError:
+        print("Not a valid JSON")
