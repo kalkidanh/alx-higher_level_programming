e775242c3dd891f080fdd454d7502e518fa9c3db
OZYMAYNDIAS <kalkidanh89@gmail.com>
2023-05-02 23:49:55 +0300

    got commits

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Script that returns the last 10 commits using GitHub API"""
+
+from sys import argv
+import requests
+
+
+if __name__ == '__main__':
+    url = 'https://api.github.com/repos/{}/{}/commits'.format(
+        argv[2], argv[1])
+    request = requests.get(url)
+    res = request.json()
+    for linr in res[:10]:
+        print('{}: {}'.format(line.get('sha'),
+                              line.get('commit').get('author').get('name')))i
