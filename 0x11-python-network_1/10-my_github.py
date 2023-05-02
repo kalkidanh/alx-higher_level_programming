f0873a626838f3f416a3cc7f9b52adee92684a85
OZYMAYNDIAS <kalkidanh89@gmail.com>
2023-05-02 23:07:48 +0300

    user id printed

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Script that takes a GitHub user name and password
+and displays its id using GitHub api"""
+
+import requests
+from sys import argv
+from requests.auth import HTTPBasicAuth
+
+
+if __name__ == "__main__":
+    user = 'https://api.github.com/user'
+    author = HTTPSBasicAuth(argv[1], argv[2])
+    req = requests.get(user, auth=author)
+    print(req.json().get('id'))
