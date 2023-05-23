ddbe77a4488c6c2eb49ce26ef0653a325a742c20
kalkidanh <kalkidanh89@gmail.com>
2023-05-23 22:07:57 +0300

    file system

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-const fpath = require('fpath');
-fpath.readFile(process.argv[2], 'utf8', function (error, content) {
+const fs = require('fs');
+fs.readFile(process.argv[2], 'utf8', function (error, content) {
 	console.log(error || content);
 });
