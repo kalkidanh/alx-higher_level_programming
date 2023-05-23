e36708e954f9b127f2d5adaad107165d4221276a
kalkidanh <kalkidanh89@gmail.com>
2023-05-23 23:04:59 +0300

    status code displayed

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,5 +1,9 @@
 #!/usr/bin/node
 const request = require('request');
-request.get(process.argv[2]).on('response', function (response) {
-  console.log(`code: ${response.statusCode}`);
+request.get(process.argv[2]), (error, response) => {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log(`code: ${response.statusCode}`);
+  }
 });
