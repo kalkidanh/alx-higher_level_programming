e6721c0e768d1c5748e4f281634793b23922b436
kalkidanh <kalkidanh89@gmail.com>
2023-05-31 23:17:41 +0300

    Jquery tasks complete

---
+++ 0x15-javascript-web_jquery/2-script.js
@@ -0,0 +1,3 @@
+$('Div#red_header').click(function() {
+	$('header').css('color', '#FF0000');
+});
---
+++ 0x15-javascript-web_jquery/5-script.js
@@ -0,0 +1,3 @@
+$('DIv#add_item').click(function() {
+	$('UL.my_list').append('<li>Item</li>');
+});
